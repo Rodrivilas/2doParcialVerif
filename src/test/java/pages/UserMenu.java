1c4fb7bbdc46407f358df2a620186ae803ce0579
Rodrigo Viladegut <rodriviladegut@gmail.com>
2022-03-10 07:32:21 -0400

    Initial commit

---
+++ src/test/java/pages/UserMenu.java
@@ -0,0 +1,10 @@
+package pages;
+
+import control.Button;
+import control.Image;
+import org.openqa.selenium.By;
+
+public class UserMenu {
+    public Image userImg=new Image(By.xpath("//img[@class=\"user_avatar big settings_avatar\"]"));
+    public Button logoutButton = new Button(By.xpath("//button[position() = 2]//span[@class=\"user_menu_label\"]"));
+}
